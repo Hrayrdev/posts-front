f111a34bf8c40c2b022f790d4b6c445f201e9236
Robert <robersargsyan2023@gmail.com>
2024-08-16 10:29:40 +0300

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   base: '/posts-front/',
   resolve: {
@@ -16,7 +11,25 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    outDir: 'dist'
-  }
+
 })
+
+// build: {
+//   outDir: 'dist'
+// npm run build
+//  cd dist
+//git init
+//git add -A
+//git commit -m 'deploy'
+//git push -f 'deploy' git@github.com:Hrayrdev/posts-front.git master:gh-pages
+//cd -
+// }ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f ~/.ssh/id_rsa_personal
+// import { defineConfig } from 'vite'
+//
+// export default defineConfig({
+//   base: '/posts-front/',
+//   остальные настройки
+// })
+// module.exports = {
+//   publicPath: '/posts-front/'
+// }
